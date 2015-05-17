239cdcb64ee34f0b5c17ae833d7640ea542fa662
nickgenov <nickolay.r.genov@gmail.com>
2015-05-17 20:45:02 +0300

    Solved a problem.

--- 04.Advanced-CSharp-Regular-Expressions-Homework/03.ExtractEmails/ExtractEmails.cs
+++ 04.Advanced-CSharp-Regular-Expressions-Homework/03.ExtractEmails/ExtractEmails.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 
@@ -9,6 +10,15 @@ class ExtractEmails
 {
     static void Main()
     {
+        string text = Console.ReadLine();
 
+        string pattern = @"[A-Za-z0-9]+([._-])?([A-Za-z0-9]+)?@([A-Za-z0-9-]+\.[A-Za-z]+)(\.[A-Za-z]+)?";
+        Regex regex = new Regex(pattern);
+        MatchCollection validEmails = regex.Matches(text);
+
+        foreach (var email in validEmails)
+        {
+            Console.WriteLine(email);
+        }
     }
 }
--- 04.Advanced-CSharp-Regular-Expressions-Homework/04.SentenceExtractor/SentenceExtractor.cs
+++ 04.Advanced-CSharp-Regular-Expressions-Homework/04.SentenceExtractor/SentenceExtractor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 
@@ -9,6 +10,22 @@ class SentenceExtractor
 {
     static void Main()
     {
+        string keyword = Console.ReadLine();
+        string text = Console.ReadLine();
 
+        string pattern = @"[\w\s’]+(–)?[\w\s’]+[!.?]";
+        Regex regex = new Regex(pattern);
+        MatchCollection validSentences = regex.Matches(text);
+
+        Regex findKeyword = new Regex(keyword);
+
+        foreach (var sentence in validSentences)
+        {
+            string currentSentence = sentence.ToString();
+            if (findKeyword.IsMatch(currentSentence))
+            {
+                Console.WriteLine(currentSentence.Trim());
+            }
+        }
     }
 }
