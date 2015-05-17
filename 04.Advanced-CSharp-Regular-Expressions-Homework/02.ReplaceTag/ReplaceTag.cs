24cf3433742bf9705715ed639ef8b2f23bd5eb19
nickgenov <nickolay.r.genov@gmail.com>
2015-05-17 19:43:06 +0300

    Solved a problem.

--- 04.Advanced-CSharp-Regular-Expressions-Homework/02.ReplaceTag/ReplaceTag.cs
+++ 04.Advanced-CSharp-Regular-Expressions-Homework/02.ReplaceTag/ReplaceTag.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 
@@ -9,6 +10,23 @@ class ReplaceTag
 {
     static void Main()
     {
+        string text = "<ul><li><a href=http://softuni.bg>SoftUni</a></li></ul>";
+        string pattern = @"<a href=http:\/\/[A-Za-z0-9]+\.[A-Za-z0-9]+>[A-Za-z0-9]+<\/a>";
 
+        string aStartPattern = @"<a href=";
+        string aEndPattern = @"</a>";
+
+        Regex regex = new Regex(pattern);
+        if (regex.IsMatch(text))
+        {
+            string result = text;
+            result = result.Replace(aStartPattern, "[URL href=");
+            result = result.Replace(aEndPattern, "[/URL]");
+            Console.WriteLine(result);
+        }
+        else
+        {
+            Console.WriteLine("Invalid input");
+        }
     }
 }
