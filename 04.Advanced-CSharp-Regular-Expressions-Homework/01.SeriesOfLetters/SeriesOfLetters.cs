bd2fd9912c5c0ed7f4ec98afdccf3566367a9577
nickgenov <nickolay.r.genov@gmail.com>
2015-05-17 19:15:50 +0300

    Solved a problem.

--- 04.Advanced-CSharp-Regular-Expressions-Homework/01.SeriesOfLetters/SeriesOfLetters.cs
+++ 04.Advanced-CSharp-Regular-Expressions-Homework/01.SeriesOfLetters/SeriesOfLetters.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 
@@ -9,6 +10,19 @@ class SeriesOfLetters
 {
     static void Main()
     {
+        string text = Console.ReadLine();
+        string pattern = @"([A-Za-z])\1{1,}";
 
+        Regex regex = new Regex(pattern);
+        MatchCollection matches = regex.Matches(text);
+
+        string result = text;
+        foreach (var match in matches)
+        {
+            char replaceChar = match.ToString()[0];
+            string matchReplace = replaceChar.ToString();
+            result = result.Replace(match.ToString(), matchReplace);
+        }
+        Console.WriteLine(result);
     }
 }
