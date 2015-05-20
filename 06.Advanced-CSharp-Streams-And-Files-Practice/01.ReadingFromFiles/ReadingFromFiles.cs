e998c9f72094a2eeeac4327fbca7d9817a4f7572
nickgenov <nickolay.r.genov@gmail.com>
2015-05-20 21:51:00 +0300

    Adding practice for lecture 6 - Streams and Files.

---
+++ 06.Advanced-CSharp-Streams-And-Files-Practice/01.ReadingFromFiles/ReadingFromFiles.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+class ReadingFromFiles
+{
+    static void Main()
+    {
+        FileStream stream = new FileStream("text.txt", FileMode.Open);
+
+        while (true)
+        {
+            int readByte = stream.ReadByte();
+            //-1 means that no byte was read (end of file)
+            if (readByte == -1)
+            {
+                break;
+            }
+            Console.WriteLine((char)readByte);
+        }
+    }
+}
---
+++ 06.Advanced-CSharp-Streams-And-Files-Practice/02.BufferredReadingFromFiles/BufferredReadingFromFiles.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+class BufferredReadingFromFiles
+{
+    static void Main()
+    {
+        FileStream stream = new FileStream("text.txt", FileMode.Open);
+
+        byte[] buffer = new byte[4096];
+
+        while (true)
+        {
+            //this time readBytes contains the NUMBER of bytes that
+            //were stored in the buffer array
+            int readBytes = stream.Read(buffer, 0, buffer.Length);
+            if (readBytes == 0)
+            {
+                break;
+            }
+            foreach (var storedByte in buffer)
+            {
+                Console.Write((char)storedByte);
+            }
+        }
+    }
+}
