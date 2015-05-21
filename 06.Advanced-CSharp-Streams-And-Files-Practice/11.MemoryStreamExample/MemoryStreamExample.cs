b3ec99202bdab557d1cd6ad44fa568697dc631ef
nickgenov <nickolay.r.genov@gmail.com>
2015-05-21 22:27:47 +0300

    Practice.

---
+++ 06.Advanced-CSharp-Streams-And-Files-Practice/11.MemoryStreamExample/MemoryStreamExample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+class MemoryStreamExample
+{
+    static void Main()
+    {
+        string text = "Text in memory";
+        byte[] bytes = Encoding.UTF8.GetBytes(text);
+
+        using (MemoryStream memoryStream = new MemoryStream(bytes))
+        {
+            while (true)
+            {
+                int readByte = memoryStream.ReadByte();
+                if (readByte == -1)
+                {
+                    break;
+                }
+
+                Console.WriteLine((char)readByte);
+            }
+        }
+    }
+}
