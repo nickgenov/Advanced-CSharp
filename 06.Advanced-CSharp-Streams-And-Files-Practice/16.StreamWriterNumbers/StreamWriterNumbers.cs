2b60f47343d72995923f736679084df25f68a36d
nickgenov <nickolay.r.genov@gmail.com>
2015-05-23 18:36:37 +0300

    Practice.

---
+++ 06.Advanced-CSharp-Streams-And-Files-Practice/16.StreamWriterNumbers/StreamWriterNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+class StreamWriterNumbers
+{
+    static void Main()
+    {
+        using (StreamWriter writer = new StreamWriter("../../numbers.txt"))
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                writer.WriteLine(i);
+            }
+        }
+
+    }
+}
+
---
+++ 06.Advanced-CSharp-Streams-And-Files-Practice/17.StopWatchMeasurements/StopWatchMeasurements.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+
+class StopWatchMeasurements
+{
+    static void Main()
+    {
+        Stopwatch timer = new Stopwatch();        
+
+        using (StreamWriter writer = new StreamWriter("file.txt"))
+        {
+            writer.AutoFlush = true;
+
+            timer.Start();
+            for (int i = 0; i < 1000000; i++)
+            {
+                writer.WriteLine(i);
+            }
+            timer.Stop();
+            Console.WriteLine("With AutoFlush: {0}", timer.Elapsed);
+        }
+
+        using (StreamWriter writer = new StreamWriter("file.txt"))
+        {
+            timer.Reset();
+            timer.Start();
+            for (int i = 0; i < 1000000; i++)
+            {
+                writer.WriteLine(i);
+            }
+            timer.Stop();
+            Console.WriteLine("Buffered Write: {0}", timer.Elapsed);
+        }
+    }
+}
