f87bc9075d59627a3eb117da3968607ff043a97c
nickgenov <nickolay.r.genov@gmail.com>
2015-05-25 15:45:18 +0300

    Solved Streams and Files homework problems.

---
+++ 06.Advanced-CSharp-Streams-And-Files-Homework/04.CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+class CopyBinaryFile
+{
+    static void Main()
+    {
+        using (FileStream source = new FileStream("K2.jpg", FileMode.Open))
+        {
+            using (FileStream destination = new FileStream("K2-copy.jpg", FileMode.Create))
+            {
+                byte[] buffer = new byte[4096];
+                while (true)
+                {
+                    int readBytes = source.Read(buffer, 0, buffer.Length);
+                    if (readBytes == 0)
+                    {
+                        break;
+                    }
+                    destination.Write(buffer, 0, readBytes);
+                }
+            }
+        }
+    }
+}
---
+++ 06.Advanced-CSharp-Streams-And-Files-Homework/05.SlicingFile/SlicingFile.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+class SlicingFile
+{
+    static void Main()
+    {
+        string sourceFile = "../../files/K2.jpg";
+        string destinationDirectory = "../../files/";
+        int parts = 4;
+
+        List<string> files = new List<string>() {
+            "Part-0.jpg",
+            "Part-1.jpg",
+            "Part-2.jpg",
+            "Part-3.jpg",
+        };
+
+        Slice(sourceFile, destinationDirectory, parts);
+        Assemble(files, destinationDirectory);
+
+    }
+    public static void Slice(string sourceFile, string destinationDirectory, int parts)
+    {
+        string fileExtension = GetFileExtension(sourceFile);
+        using (FileStream source = new FileStream(sourceFile, FileMode.Open))
+        {
+            long bytesInSource = source.Length;
+            double bytesPerPart = Math.Ceiling(bytesInSource / (double)parts);
+
+            for (int partNumber = 0; partNumber < parts; partNumber++)
+            {
+                using (FileStream destination = new FileStream(string.Format("{0}Part-{1}{2}", destinationDirectory, partNumber, fileExtension), FileMode.Create))
+                {
+                    byte[] buffer = new byte[4096];
+                    long bytesWrittenToPart = 0;
+                    while (true)
+                    {
+                        int readBytes = source.Read(buffer, 0, buffer.Length);
+                        if (readBytes == 0)
+                        {
+                            break;
+                        }
+                        destination.Write(buffer, 0, readBytes);
+                        bytesWrittenToPart += readBytes;
+                        if (bytesWrittenToPart >= bytesPerPart)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public static void Assemble(List<string> files, string destinationDirectory)
+    {
+        string fileExtension = string.Empty;
+        if (files.Count > 0)
+        {
+            fileExtension = GetFileExtension(files[0]);
+        }
+
+        using (FileStream destination = new FileStream(string.Format("{0}assembled{1}", destinationDirectory, fileExtension), FileMode.Create))
+        {
+            foreach (var file in files)
+            {
+                using (FileStream source = new FileStream(string.Format("{0}{1}", destinationDirectory, file), FileMode.Open))
+                {
+                    byte[] buffer = new byte[4096];
+                    while (true)
+                    {
+                        int readBytes = source.Read(buffer, 0, buffer.Length);
+                        if (readBytes == 0)
+                        {
+                            break;
+                        }
+                        destination.Write(buffer, 0, readBytes);
+                    }
+                }
+            }
+        }
+    }
+    public static string GetFileExtension(string sourceFile)
+    {
+        string pattern = @"\.[\w]+";
+        Regex regex = new Regex(pattern);
+        Match fileExtension = regex.Match(sourceFile);
+        return fileExtension.ToString();
+    }
+}
